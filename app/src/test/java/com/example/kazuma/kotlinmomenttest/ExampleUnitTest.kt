24dad8730292789f531f260b1f0768ca1b4ece79
miura <noblesse-oblige_23@i.softbank.jp>
2016-05-28 10:06:07 +0900

    feat(project): create

---
+++ app/src/test/java/com/example/kazuma/kotlinmomenttest/ExampleUnitTest.kt
@@ -0,0 +1,16 @@
+package com.example.kazuma.kotlinmomenttest
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    Test
+    Throws(Exception::class)
+    public fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
